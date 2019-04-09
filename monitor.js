0aed8d11cfe0f141e4b4876356618ce2fad3589c
Daniel Ivanov <daniel@limechain.tech>
2019-04-09 15:26:11 +0300

    add transactions info

--- monitor.js
+++ monitor.js
@@ -5,6 +5,7 @@ const ethers = require('ethers');
 
 const EVERY_THREE_SECONDS = "*/3 * * * * *";
 
+const TRANSACTION_STATUSES = require('./constants.json').TRANSACTION_STATUSES;
 const PAYMENT_STATUSES = require('./constants.json').PAYMENT_STATUSES;
 const PAYMENT_TYPES = require('./constants.json').PAYMENT_TYPES;
 
@@ -21,38 +22,34 @@ class Monitor {
      * @param {string} paymentId LimePay payment ID
      */
     monitor(paymentId) {
-        console.log(`Starting monitoring payment with id: ${paymentId}`);
+        console.log(`Started monitoring payment with id: ${paymentId}`);
 
         // The function is being executed every 3 seconds until `successful` or `failed` status is retrieved
         const scheduledJob = schedule.scheduleJob(paymentId, EVERY_THREE_SECONDS, async () => {
             try {
                 // GET the payment from LimePay
-                const result = await this.limepay.payments.get(paymentId);
+                const retrievedPayment = await this.limepay.payments.get(paymentId);
 
-                // Update the status of the Payment in firestore
+                // Get the Payment from DB
                 const paymentInDB = await FirestoreService.getPayment(paymentId);
-                await FirestoreService.setPayment({ ...paymentInDB, status: result.status });
-
                 // Get the Job from DB
                 let job = await FirestoreService.getJob(paymentInDB.jobId);
 
-                if (shouldAddEscrowActionLog(job, paymentInDB, result)) {
-                    await updateAuthoriseEscrowActionLog(job, paymentInDB, result.genericTransactions[0]);
+                if (isJobCreation(paymentInDB.type)) {
+                    await updateJobCreationPayment(job, paymentInDB, retrievedPayment);
+                } else if (isJobCompletion(paymentInDB.type)) {
+                    await updateJobCompletionPayment(job, paymentInDB, retrievedPayment);
+                } else {
+                    throw 'Invalid payment type';
                 }
 
                 // Stop the monitoring of the status is Successful or Failed
-                if (result.status === PAYMENT_STATUSES.SUCCESSFUL || result.status === PAYMENT_STATUSES.FAILED) {
+                if (isTerminalStatus(retrievedPayment.status)) {
                     // Stop the monitoring of the payment
-                    console.log(`Stopping monitoring of payment with ID ${paymentId}, for Job with ID ${paymentInDB.jobId}. Status is ${result.status}`);
+                    console.log(`Stopping monitoring of payment with ID ${paymentId}, for Job with ID ${paymentInDB.jobId}. Status is ${retrievedPayment.status}`);
                     scheduledJob.cancel();
-
-                    saveTransactionHash(paymentInDB, result);
-
-                    if (result.status === PAYMENT_STATUSES.SUCCESSFUL) {
-                        await updateState(job, paymentInDB);
-                        await updateActionLog(job, paymentInDB);
-                    }
                 }
+
             } catch (error) {
                 console.log(`An error occured while monitoring payment with ID ${paymentId}. Error: ${error}`);
             }
@@ -65,66 +62,319 @@ class Monitor {
 module.exports = Monitor;
 
 /**
- * @name updateState
- * @description Updates the state of the Job to the next one. If job creation -> 'Funds In Escrow'. if job completion -> `Complete`
- * @param {object} payment 
+ * @name isJobCreation
+ * @param {String} type
+ * @description Returns true if the type of the payment is JOB_CREATION 
  */
-const updateState = async function (job, payment) {
-    if (payment.type === PAYMENT_TYPES.JOB_CREATION) {
-        job.state = 'Funds In Escrow';
-    } else if (payment.type === PAYMENT_TYPES.JOB_COMPLETION) {
-        job.state = 'Complete';
-    }
-    await FirestoreService.setJob(job);
-    console.log(`updated job state`, job.state);
+const isJobCreation = function (type) {
+    return type === PAYMENT_TYPES.JOB_CREATION;
 }
 
 /**
- * @name updateActionLog
- * @description Updates the action log of the Job. If job creation -> 'Authorise escrow'. if job completion -> `Complete job`
- * @param {object} payment
- * @param {object} job
+ * @name isJobCompletion
+ * @param {String} type
+ * @description Returns true if the type of the payment is JOB_CREATION 
  */
-const updateActionLog = async function (job, payment) {
+const isJobCompletion = function (type) {
+    return type === PAYMENT_TYPES.JOB_COMPLETION;
+}
 
-    let actionLog = { timestamp: moment().format('x'), message: '', executedBy: 'User', private: false };
-    if (payment.type === PAYMENT_TYPES.JOB_CREATION) {
-        Object.assign(actionLog, { type: 'Send CAN to escrow' });
-    } else if (payment.type === PAYMENT_TYPES.JOB_COMPLETION) {
-        Object.assign(actionLog, { type: 'Complete job'});
+/**
+ * @name updateJobCreationPayment
+ * @param {Object} job 
+ * @param {Object} paymentInDB
+ * @param {object} retrievedPayment
+ * @summary Updates the state of the job and Create Job payment in the DB depending on the progress 
+ */
+const updateJobCreationPayment = async function (job, payment, retrievedPayment) {
+    const approveTx = retrievedPayment.genericTransactions[0];
+    const createJobTx = retrievedPayment.genericTransactions[1];
+    
+    // 1. Update Approve Transction Data
+    updateApproveTransactionData(approveTx, payment, job);
+
+    // 2. Update CreateJob transaction Data
+    updateCreateJobTxData(createJobTx, payment, job);
+    
+    // 3. Update Payment State
+    payment.status = retrievedPayment.status;
+    await FirestoreService.setPayment(payment);
+
+    // 4. Update Job State on success
+    if (payment.status == PAYMENT_STATUSES.SUCCESSFUL) {
+        job.state = 'Funds In Escrow';
     }
-    console.log("actionLog:", actionLog);
-
-    job.actionLog.push(actionLog);
     await FirestoreService.setJob(job);
 }
 
-const updateAuthoriseEscrowActionLog = async function (job, paymentInDB, transaction) {
-    const actionLog = {
-        timestamp: moment().format('x'),
-        amountCan: convertWithDecimals(transaction.functionParams[1].value),
-        type: 'Authorise escrow',
-        executedBy: 'User',
-        message: '',
-        private: true
+
+/**
+ * @name updateJobCompletionPayment
+ * @param {Object} job
+ * @param {Object} paymentInDB
+ * @summary Updates the state of the job and Complete Job payment in the DB depending on the progress
+ */
+const updateJobCompletionPayment = async function (job, payment, retrievedPayment) {
+    const completeJobTX = retrievedPayment.genericTransactions[0];
+
+    if (isTransactionBroadcasted(completeJobTX)) {
+        appendCompleteJobTX(payment, completeJobTX);
+    }
+    else if (isTransactionMined(completeJobTX)) {
+        // Safety check. If we missed an update and did not added the approve transaction.
+        safetyCheckCompleteTx(payment, completeJobTX);
+
+        // Update transaction success to true
+        updateTransactionByHash(payment, completeJobTX.transactionHash, true, false);
+
+        // // Adds new Job Action when Approve TX is mined
+        if (shouldAddCompleteJobActionLog(job, completeJobTX)) {
+            appendCompleteJobActionLog(job);
+            console.log(`Complete Job TX for payment [${payment.id}] was mined. TX hash: ${completeJobTX.transactionHash}`);
+        }
+    }
+    else if (isTransactionReverted(completeJobTX)) {
+        // Safety check. If we missed an update and did not added the approve transaction.
+        safetyCheckCompleteTx(payment, completeJobTX);
+
+        // Update transaction failure to true
+        updateTransactionByHash(payment, completeJobTX.transactionHash, false, true);
+        console.log(`Complete Job TX for payment [${payment.id}] has reverted. TX hash: ${completeJobTX.transactionHash}`);
+    }
+
+    // 3. Update Payment State
+    payment.status = retrievedPayment.status;
+    await FirestoreService.setPayment(payment);
+
+    // 4. Update Job State on success
+    if (payment.status == PAYMENT_STATUSES.SUCCESSFUL) {
+        job.state = 'Complete';
+    }
+    await FirestoreService.setJob(job);
+}
+
+// ------------- Main Functions -----------------
+
+function updateCreateJobTxData(createJobTx, payment, job) {
+    if (isTransactionBroadcasted(createJobTx)) {
+        appendCreateJobTx(payment, createJobTx);
+    }
+    else if (isTransactionMined(createJobTx)) {
+        // Safety check. If we missed an update and did not added the createJob transaction.
+        safetyCheckCreateJobTx(payment, createJobTx);
+        
+        //Update transaction success to true
+        updateTransactionByHash(payment, createJobTx.transactionHash, true, false);
+
+        // Add new Job Action when CreateJob TX is mined
+        if (shouldAddSendCANActionLog(job, createJobTx)) {
+            appendSendCANActionLog(job);
+            console.log(`Create Job TX for payment [${payment.id}] was mined. TX hash: ${createJobTx.transactionHash}`);
+        }
+    }
+    else if (isTransactionReverted(createJobTx)) {
+        // Safety check. If we missed an update and did not added the createJob transaction.
+        safetyCheckCreateJobTx(payment, createJobTx);
+
+        //Update transaction failure to true
+        updateTransactionByHash(payment, createJobTx.transactionHash, false, true);
+        console.log(`Create Job TX for payment [${payment.id}] rhas everted. TX hash: ${createJobTx.transactionHash}`);
+    }
+}
+
+function updateApproveTransactionData(approveTx, payment, job) {
+    if (isTransactionBroadcasted(approveTx)) {
+        appendApproveTransaction(payment, approveTx);
+    }
+    else if (isTransactionMined(approveTx)) {
+        // Safety check. If we missed an update and did not added the approve transaction.
+        safetyCheckApproveTx(payment, approveTx);
+        
+        // Update transaction success to true
+        updateTransactionByHash(payment, approveTx.transactionHash, true, false);
+        
+        // Adds new Job Action when Approve TX is mined
+        if (shouldAddEscrowActionLog(job, approveTx)) {
+            appendAuthoriseEscrowActionLog(job, approveTx);
+            console.log(`Approve TX for payment [${payment.id}] mined. TX hash: ${approveTx.transactionHash}`);
+        }
+    }
+    else if (isTransactionReverted(approveTx)) {
+        // Safety check. If we missed an update and did not added the approve transaction.
+        safetyCheckApproveTx(payment, approveTx);
+        // Update transaction failure to true
+        updateTransactionByHash(payment, approveTX.transactionHash, false, true);
+        console.log(`Approve TX for payment [${payment.id}] reverted. TX hash: ${approveTx.transactionHash}`);
+    }
+}
+
+// ------------- Main Functions -----------------
+
+// ------------- Transaction Functions ----------
+
+// Safety check. If we missed an update and did not added the approve transaction.
+function safetyCheckApproveTx(payment, approveTx) {
+    if (payment.transactions == undefined || !findTransactionByHash(payment, approveTx.transactionHash)) {
+        appendApproveTransaction(payment, approveTx);
+    }
+}
+
+// Safety check. If we missed an update and did not added the create job transaction.
+function safetyCheckCreateJobTx(payment, createJobTx) {
+    if (!findTransactionByHash(payment, createJobTx.transactionHash)) {
+        appendCreateJobTx(payment, createJobTx);
+    }
+}
+
+// Safety check. If we missed an update and did not added the complete job transaction.
+function safetyCheckCompleteTx(payment, completeJobTX) {
+    if (payment.transactions == undefined || !findTransactionByHash(payment, completeJobTX.transactionHash)) {
+        appendCompleteJobTX(payment, completeJobTX);
+    }
+}
+
+// Adds the Approve transaction info into limepay-payment entry 
+function appendApproveTransaction(payment, approveTransaction) {
+    if (payment.transactions) {
+        const approveTX = findTransactionByHash(payment, approveTransaction.transactionHash);
+        // Transaction is already added in transactions array. Will skip adding it again
+        if (approveTX) return;
+    } else {
+        payment.transactions = [];
+        const transaction = createNewTransaction(approveTransaction.transactionHash, 'Authorise escrow');
+        payment.transactions.push(transaction);
+        console.log(`Approve TX for payment [${payment.id}] broadcasted. TX hash: ${approveTransaction.transactionHash}`);
+    }
+}
+
+// Adds the CreateJob transaction into limepay-payment entry
+function appendCreateJobTx(payment, createJobTransactionFromLP) {
+    const createJobTx = findTransactionByHash(payment, createJobTransactionFromLP.transactionHash);
+    // Transaction is already added in transactions array. Will skip adding it again
+    if (createJobTx) return;
+
+    const transaction = createNewTransaction(createJobTransactionFromLP.transactionHash, 'Send CAN to escrow');
+    payment.transactions.push(transaction);
+    console.log(`Create Job TX for payment [${payment.id}] was broadcasted. TX hash: ${createJobTransactionFromLP.transactionHash}`);
+}
+
+// Adds the Complete Job transaciton info into limepay-payment entry
+function appendCompleteJobTX(payment, completeJobTX) {
+    if (payment.transactions) {
+        const completeJob = findTransactionByHash(payment, completeJobTX.transactionHash);
+         // Transaction is already added in transactions array. Will skip adding it again
+        if (completeJob) return;
+    } else {
+        payment.transactions = [];
+        const transaction = createNewTransaction(completeJobTX.transactionHash, 'Complete job');
+        payment.transactions.push(transaction);
+        console.log(`Complete Job TX for payment [${payment.id}] was broadcasted. TX hash: ${completeJobTX.transactionHash}`);
+    }
+}
+
+// Finds a given transaciton by its transaction hash
+function findTransactionByHash(payment, transactionHash) {
+    return payment.transactions.find(tx => { return tx.hash === transactionHash; });
+}
+
+function updateTransactionByHash(payment, transactionHash, success, failure) {
+    const tx = findTransactionByHash(payment, transactionHash);
+    tx.success = success;
+    tx.failure = failure;
+}
+
+function isTerminalStatus(status) {
+    return status === PAYMENT_STATUSES.SUCCESSFUL || status === PAYMENT_STATUSES.FAILED;
+}
+
+// Create new transaction object
+function createNewTransaction(transactionHash, actionType) {
+    return {
+        hash: transactionHash,
+        actionType: actionType,
+        failure: false,
+        success: false,
+        timestamp: moment().format('x')
     };
-    console.log("actionLog:", actionLog);
-    job.actionLog.push(actionLog);
-    await FirestoreService.setJob(job);
 }
 
+// ------------- Transaction Functions ----------
 
-const saveTransactionHash = async function (paymentInDb, limePayPayment) {
-    // If the payment is Job creation, we have 2 transactions. Approve and CreateJob. We want to save the CreateJob hash
-    if (paymentInDb.type === PAYMENT_TYPES.JOB_CREATION) {
-        paymentInDb.txHash = limePayPayment.genericTransactions[1].transactionHash;
-    } else if (paymentInDb.type === PAYMENT_TYPES.JOB_COMPLETION) {
-        // If the payment is Job completion, we have 1 transaction - CompleteJob
-        paymentInDb.txHash = limePayPayment.genericTransactions[0].transactionHash;
+
+// -------------- Job Actions ------------------- 
+
+const appendAuthoriseEscrowActionLog = function (job, transaction) {
+    let action = createNewAction('Authorise escrow', true);
+    action.amountCan = convertWithDecimals(transaction.functionParams[1].value);
+
+    console.log("actionLog:", action);
+    job.actionLog.push(action);
+}
+
+const appendSendCANActionLog = function (job) {
+    const action = createNewAction('Send CAN to escrow', false);
+    console.log('ActionLog:', action);
+    job.actionLog.push(action);
+}
+
+const appendCompleteJobActionLog = function (job) {
+    let action = createNewAction('Complete job', false);
+    console.log("actionLog:", action);
+    job.actionLog.push(action);
+}
+
+const createNewAction = function (type, private, executedBy = 'User') {
+    const action = {
+        type,
+        timestamp: moment().format('x'),
+        message: '',
+        private,
+        executedBy
     }
-    await FirestoreService.setPayment(paymentInDb);
+    return action;
 }
 
+// Updates the Job action log when the approve transaction is mined
+function shouldAddEscrowActionLog(job, transaction) {
+    const lastActionLog = job.actionLog[job.actionLog.length - 1];
+    // Check whether we already added the escrow action log
+    return lastActionLog.type == 'Accept terms' && transaction.status === PAYMENT_STATUSES.SUCCESSFUL;
+}
+
+// Updates the Job action log when the approve transaction is mined
+function shouldAddSendCANActionLog(job, transaction) {
+    const lastActionLog = job.actionLog[job.actionLog.length - 1];
+    // Check whether we already added the escrow action log
+    return lastActionLog.type == 'Authorise escrow' && transaction.status === PAYMENT_STATUSES.SUCCESSFUL;
+}
+
+// Updates the Job action log when the approve transaction is mined
+function shouldAddCompleteJobActionLog(job, transaction) {
+    const lastActionLog = job.actionLog[job.actionLog.length - 1];
+    // Check whether we already added the escrow action log
+    return lastActionLog.type != 'Complete job' && transaction.status === PAYMENT_STATUSES.SUCCESSFUL;
+}
+
+// -------------- Job Actions -------------- 
+
+
+
+// -------------- UTILS --------------------
+
+// Returns true if the transaction is broadcasted
+function isTransactionBroadcasted(transaction) {
+    return transaction.status === TRANSACTION_STATUSES.PROCESSING && transaction.transactionHash;
+}
+
+// Returns true if the transaction is mined
+function isTransactionMined(transaction) {
+    return transaction.status === TRANSACTION_STATUSES.SUCCESSFUL && transaction.transactionHash;
+}
+
+// Returns true if the transaction has reverted
+function isTransactionReverted(transaction) {
+    return transaction.status === TRANSACTION_STATUSES.FAILED && transaction.transactionHash;
+}
 
 // Converts the Amount of CAN tokens to their base. CAN token has 6 decimals. 
 const convertWithDecimals = function (amount) {
@@ -132,12 +382,5 @@ const convertWithDecimals = function (amount) {
     return ethers.utils.formatUnits(amountInBN, 6).toString();
 }
 
-function shouldAddEscrowActionLog(job, paymentInDB, result) {
-    const lastActionLog = job.actionLog[job.actionLog.length - 1];
+// -------------- UTILS --------------------
 
-    // Check whether we already added the escrow action log
-    if (lastActionLog.type != 'Authorise escrow') { 
-        return paymentInDB.type === PAYMENT_TYPES.JOB_CREATION && result.genericTransactions[0].status === PAYMENT_STATUSES.SUCCESSFUL;
-    }
-    return false;
-}
